c6bde5638ce09a2096eb222810df02b89163df94
morena <web.iamsrn@gmail.com>
2022-11-08 16:19:34 +0400

    wrap with modals all app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import ModalContext from './components/context/ModalContext';
 import NavBar from "./components/structure/NavBar";
 import Routing from "./components/structure/Routing";
+import {useState} from "react";
+import modals from "./util/modalStatesDict.json";
 
 const App = () => {
 
-  return (
-    <div>
-        <NavBar/>
-        <Routing/>
-    </div>
-  );
+    const [infoMess, setInfoMess] = useState([]);
+    const [modalState, setModalState] = useState(modals.close);
+
+    //модалка информации
+    const handleShowModalInfo = () => {
+        setModalState(modals.infoOpen);
+    }
+    //закрытие
+    const handleClose = () => {
+        setModalState(modals.close);
+    }
+
+    return (
+        <ModalContext.Provider value={{
+            infoMess : infoMess,
+            setInfoMess : setInfoMess,
+            modalState : modalState,
+            handleShowModalInfo : handleShowModalInfo,
+            handleClose : handleClose,
+        }}>
+            <NavBar/>
+            <Routing/>
+        </ModalContext.Provider>
+    );
 };
 
 export default App;
--- src/components/structure/Routing.js
+++ src/components/structure/Routing.js
@@ -1,14 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import ErrorPattern from "./pages/ErrorPattern";
-import ModalWrapper from "./pages/logic/ModalWrapper";
+import Nets from "./pages/logic/Nets";
 
 const Routing = () => {
   return(
       <div className="container mt-3">
           <Routes>
               <Route path="/" element={<About/>} />
-              <Route path="/nets" element={<ModalWrapper/>} />
+              <Route path="/nets" element={<Nets/>} />
               <Route path="/*" element={<ErrorPattern/>} />
           </Routes>
       </div>
